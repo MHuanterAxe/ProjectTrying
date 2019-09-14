9eeece75b8f5fc500f5e2a98868a3cacc307b1a9
MHuanterAxe <maks.shkm@gmail.com>
2019-09-14 15:05:47 +0300

    Added p5js

---
+++ js/clock.js
@@ -0,0 +1,10 @@
+function setup(){
+    let canvas = createCanvas(200, 200);
+    canvas.parent('center');
+}
+function draw(){
+    let hr = hour();
+    let mn = minute();
+    let sc = second();
+    
+}
